e18e126c77fb65b5d346d5d78c4a96debb949546
alexfrunza <frunza.alexandru.2004@gmail.com>
2020-04-26 11:48:13 +0300

    Add admin user and a navbar tab for add a new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,19 +8,28 @@ class GamesController < ApplicationController
   end
 
   def new
+    verify_user
     @game = Game.new
     @genre_list = params[:genre_list]
   end
 
   def create
+    verify_user
     @game = Game.new(game_params)
     @game.stock = 0
-    binding.pry
     @game.save
   end
 
   private
+
     def game_params
       params.require(:game).permit(:title, :about, :release_date, :developer_id, :franchise_id, :platform_id)
     end
+
+    def verify_user
+      unless current_user.admin?
+        redirect_to root_path, :alert => "Acces denied."
+      end
+    end
+
 end
